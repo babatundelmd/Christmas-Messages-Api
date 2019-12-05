cc61856c261a7d74442592393f850a7cf9ba07c1
Babatunde Lamidi <babatundela@ha-shem.com>
2019-12-05 16:20:15 +0100

    chore: huge refactor

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const PostSchema = mongoose.Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Message Entries', PostSchema);
