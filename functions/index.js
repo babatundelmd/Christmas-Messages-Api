278b169adc47bd49c7cdfaae18596162fdcefc96
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-01-03 14:41:19 +0100

    🚑chore: fix bug in functions

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,6 @@ const functions = require( 'firebase-functions' );
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-const functions = require( 'firebase-functions' );
 const express = require( 'express' )
 const app = express();
 
